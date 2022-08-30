2c850893c15a8c25471b43b693192035e31f0152
SpringhawkIII <99701433+SpringhawkIII@users.noreply.github.com>
2022-08-30 09:58:03 -0500

    HW-19 8-30-22

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-.console.log("Your index.js file is loaded correctly!") ;
+console.log("Your index.js file is loaded correctly!");
+$('a.nav-link').hover(function(){
+  console.log("test");
+  $(this).css('color', '#00AEEF');
+  }, function(){
+  $(this).css('color', '#ffffff');
+});
